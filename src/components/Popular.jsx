35f9bd45731cd0dbb40ff4e8fa79e09864e2c67c
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-10 01:25:14 -0700

    recipes cards popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,53 +1,47 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+// import ListCard from "./ListCard";
 
 function Popular() {
-  const [popular, setPopular] = useState([]);
+  // const [popular, setPopular] = useState([]);
 
-  useEffect(() => {
-    getPopular();
-  }, []);
+  // useEffect(() => {
+  //   getPopular();
+  // }, []);
 
-  const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
-    console.log(data.recipes);
-  };
+  // const getPopular = async () => {
+  //   const api = await fetch(
+  //     `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+  //   );
+  //   const data = await api.json();
+  //   setPopular(data.recipes);
+  //   console.log(data.recipes);
+  // };
 
   return (
-    <div>
-      {popular.map((recipe) => {
-        return (
-          <>
-            <div
-              className="card"
-              // style={width: 18rem}
-            >
+    <div className="justify-content-between row">
+      {/* {popular.map((recipe) => {
+        return ( */}
+          <div className="col">
+            {/* <ListCard curRecipe={recipe} /> */}
+
+            <div className="card m-3" style={{ width: 300, height: 300 }}>
+              <img src=""
+              // {recipe.image} 
+              className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">{recipe.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">
-                  {/* {recipe.instructions} */}
-                </p>
-                <a href="#" className="card-link">
-                  Card link
-                </a>
-                <a href="#" className="card-link">
-                  Another link
-                </a>
+                <h5 className="card-title">
+                  <a href="#" className="black">
+                    {/* {recipe.title} */}
+                    TITLE
+                  </a>
+                </h5>
               </div>
             </div>
-          </>
-
-          // <div>
-          //   <p>{recipe.title}</p>
-          // </div>
-        );
-      })}
+          </div>
+        {/* );
+      })} */}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
 
-
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div>
-        <Veggie />
+      <Veggie />
+      <div className="m-5 center">
         <Popular />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

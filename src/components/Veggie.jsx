640444315f43332a4375a0d7838371f2a4e2ca26
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-13 14:56:08 -0700

    api fetch/styling

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import RecipeCard from "./RecipeCard";
+import { Splide } from "@splidejs/react-splide";
+import styled from "styled-components";
+import "@splidejs/react-splide/css";
 
-function Veggie() {
-  return <div>Veggie</div>;
+function Veggie(recipe) {
+  const [veggie, setVeggie] = useState([]);
+
+  const Wrapper = styled.div`
+    margin: 4rem 0rem;
+  `;
+
+  useEffect(() => {
+    getVeggie();
+    console.log("Effect");
+  }, []);
+
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+      console.log(data.recipes);
+    }
+
+    // VEGAN`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegan`;
+    // VEGGIE`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`;
+  };
+  return (
+    <div className="">
+      <Wrapper>
+        <h3>Our Vegetarian Picks</h3>
+        <Splide
+          aria-label="Popular Recipes"
+          options={{
+            perPage: 3,
+            gap: "5rem",
+            // arrows: false,
+            pagination: false,
+            drag: "free",
+          }}
+        >
+          {veggie.map((recipe) => {
+            return (
+              <>
+                <RecipeCard recipe={recipe} />;
+              </>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default Veggie;

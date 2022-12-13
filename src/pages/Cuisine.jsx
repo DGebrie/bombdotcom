95cce6d0a40cd3f763275c67d977ba6b263a58c5
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-13 14:55:57 -0700

    useParams to fetch cuisine type

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    border-radius: 2rem;
+    width: 100%;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+const Cuisine = () => {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+
+  return (
+    <>
+      <h1 className="text-center mb-4">
+        <strong>{params.type}</strong>
+      </h1>
+      <Grid>
+        {cuisine.map((item) => {
+          return (
+            <Card key={item.id}>
+              <img src={item.image} alt="" />
+              <h4>{item.title}</h4>
+            </Card>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
+
+export default Cuisine;

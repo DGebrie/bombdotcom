467d6ea80661a2b7fab0f8b629b55ac20966a29b
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-15 21:20:06 -0700

    search bar form

---
+++ src/components/Search.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const Search = () => {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  };
+
+  return (
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <FaSearch />
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+        />
+      </div>
+    </FormStyle>
+  );
+};
+
+const FormStyle = styled.form`
+  margin: 0rem 20rem;
+  div {
+    width: 100%;
+    position: relative;
+  }
+  input {
+    border: none;
+    background: #313131;
+    font-size: 1rem;
+    color: white;
+    padding: 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    width: 100%;
+    outline: none;
+  }
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`;
+
+export default Search;

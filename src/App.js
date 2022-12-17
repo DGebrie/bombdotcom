45ccf8f2d269d180872916d9614d2fd9849b29a3
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-16 17:35:17 -0700

    logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Pages from "./pages/Pages";
 import { BrowserRouter, Link } from "react-router-dom";
 import Search from "./components/Search";
 import styled from "styled-components";
-import { GiSpoon } from "react-icons/gi";
+import { GiCook } from "react-icons/gi";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Nav>
           <Logo to={"/"}>
-            <GiSpoon />
+            <GiCook />
             Taste Buds
           </Logo>
         </Nav>
@@ -25,12 +25,12 @@ function App() {
 
 const Logo = styled(Link)`
   text-decoration: none;
-  font-size: 1.5rem;
+  font-size: 2.5rem;
   font-weight: 400;
   font-family: "Lobster Two", cursive;
 `;
 const Nav = styled.div`
-  padding: 4rem 0rem;
+  padding: 1rem 0rem;
   display: flex;
   justify-content: center;
   align-items: center;

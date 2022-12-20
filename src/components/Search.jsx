83fd3c78f7b09e4fd0b12b139789c93b7260178e
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-20 16:59:30 -0700

    searchbar mobile responsive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,43 +13,115 @@ const Search = () => {
   };
 
   return (
-    <FormStyle onSubmit={submitHandler}>
-      <div>
-        <FaSearch />
-        <input
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
-          value={input}
-        />
+    // <form class="input-group rounded m-0" onSubmit={submitHandler}>
+    //   <input
+    //     onChange={(e) => setInput(e.target.value)}
+    //     type="search"
+    //     className="form-control rounded text-white"
+    //     placeholder="Search"
+    //     aria-label="Search"
+    //     aria-describedby="search-addon"
+    //     value={input}
+    //     style={{
+    //       backgroundColor: "#313131",
+    //       color: "white",
+    //       border: "none",
+    //       width: 100,
+    //       text: "white",
+    //     }}
+    //   />
+    //   <button
+    //     style={{
+    //       backgroundColor: "#313131",
+    //       color: "white",
+    //     }}
+    //     class="input-group-text border-0"
+    //     id="search-addon"
+    //     type="submit"
+    //   >
+    //     <FaSearch />
+    //   </button>
+    // </form>
+
+    <div class="row mt-5" onSubmit={submitHandler}>
+      <div class="col-md-5 mx-auto">
+        <div class="input-group">
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="search"
+            className="form-control rounded text-white"
+            placeholder="Recipes"
+            aria-label="Search"
+            aria-describedby="search-addon"
+            value={input}
+            style={{
+              backgroundColor: "#313131",
+              color: "white",
+              borderRadius: 1,
+              width: 100,
+              text: "white",
+            }}
+          />
+          <span class="input-group-append">
+            <button
+              style={{
+                backgroundColor: "#313131",
+                color: "white",
+              }}
+              class="btn border-start-0 "
+              type="button"
+            >
+              <FaSearch />
+            </button>
+          </span>
+        </div>
       </div>
-    </FormStyle>
+    </div>
+    // <FormStyle onSubmit={submitHandler}>
+    //   <div>
+    //     <FaSearch />
+    //     <input
+    //       onChange={(e) => setInput(e.target.value)}
+    //       type="text"
+    //       value={input}
+    //     />
+    //   </div>
+    // </FormStyle>
   );
 };
 
-const FormStyle = styled.form`
-  margin: 0rem 20rem;
-  div {
-    width: 100%;
-    position: relative;
-  }
-  input {
-    border: none;
-    background: #313131;
-    font-size: 1rem;
-    color: white;
-    padding: 1rem 3rem;
-    border: none;
-    border-radius: 1rem;
-    width: 100%;
-    outline: none;
-  }
-  svg {
-    position: absolute;
-    top: 50%;
-    left: 0%;
-    transform: translate(100%, -50%);
-    color: white;
-  }
+const icon = styled.svg`
+  position: absolute;
+  top: 50%;
+  left: 0%;
+  transform: translate(100%, -50%);
+  color: white;
 `;
+
+// const FormStyle = styled.form`
+//   margin: 0rem 20rem;
+//   div {
+//     width: 100%;
+//     position: relative;
+//   }
+//   input {
+//     border: none;
+//     background: #313131;
+//     font-size: 1rem;
+//     color: white;
+//     padding: 1rem 3rem;
+//     border: none;
+//     border-radius: 1rem;
+//     width: 100%;
+//     outline: none;
+//   }
+//   svg {
+//     position: absolute;
+//     top: 50%;
+//     left: 0%;
+//     transform: translate(100%, -50%);
+//     color: white;
+//   }
+// `;
 
 export default Search;

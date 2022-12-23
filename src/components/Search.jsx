70af99e666d37f4cddd429f4cc1286ffdad5d426
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-12-22 19:29:42 -0800

    clean up

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,36 +13,6 @@ const Search = () => {
   };
 
   return (
-    // <form class="input-group rounded m-0" onSubmit={submitHandler}>
-    //   <input
-    //     onChange={(e) => setInput(e.target.value)}
-    //     type="search"
-    //     className="form-control rounded text-white"
-    //     placeholder="Search"
-    //     aria-label="Search"
-    //     aria-describedby="search-addon"
-    //     value={input}
-    //     style={{
-    //       backgroundColor: "#313131",
-    //       color: "white",
-    //       border: "none",
-    //       width: 100,
-    //       text: "white",
-    //     }}
-    //   />
-    //   <button
-    //     style={{
-    //       backgroundColor: "#313131",
-    //       color: "white",
-    //     }}
-    //     class="input-group-text border-0"
-    //     id="search-addon"
-    //     type="submit"
-    //   >
-    //     <FaSearch />
-    //   </button>
-    // </form>
-
     <form class="row mt-5" onSubmit={submitHandler}>
       <div class="col-md-5 mx-auto">
         <div class="input-group">
@@ -70,51 +40,7 @@ const Search = () => {
         </div>
       </div>
     </form>
-    // <FormStyle onSubmit={submitHandler}>
-    //   <div>
-    //     <FaSearch />
-    //     <input
-    //       onChange={(e) => setInput(e.target.value)}
-    //       type="text"
-    //       value={input}
-    //     />
-    //   </div>
-    // </FormStyle>
   );
 };
-
-const icon = styled.svg`
-  position: absolute;
-  top: 50%;
-  left: 0%;
-  transform: translate(100%, -50%);
-  color: white;
-`;
-
-// const FormStyle = styled.form`
-//   margin: 0rem 20rem;
-//   div {
-//     width: 100%;
-//     position: relative;
-//   }
-//   input {
-//     border: none;
-//     background: #313131;
-//     font-size: 1rem;
-//     color: white;
-//     padding: 1rem 3rem;
-//     border: none;
-//     border-radius: 1rem;
-//     width: 100%;
-//     outline: none;
-//   }
-//   svg {
-//     position: absolute;
-//     top: 50%;
-//     left: 0%;
-//     transform: translate(100%, -50%);
-//     color: white;
-//   }
-// `;
 
 export default Search;
